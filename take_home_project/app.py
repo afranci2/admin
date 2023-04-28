dbe28e71a36f8c0e1575308290d1bff163ccff86
El Samp <anthony.francisco.aoct@gmail.com>
2023-04-28 14:29:31 -0400

    AEf

--- take_home_project/app.py
+++ take_home_project/app.py
@@ -12,7 +12,7 @@ def home():
         host=os.getenv("POSTGRES_HOST"),
         port=os.getenv("POSTGRES_PORT"),
         dbname=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USE"),
+        user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD")
     )
     print("Everything connected")
